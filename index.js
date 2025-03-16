faa6e687ab0612a2f496136a76a477f73cdd484e
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-03-17 00:08:01 +0200

    Clean up code
    Continued to clean up code with the try...catch method.  Code should now be easier to read and edit if need be.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const res = await fetch("https:apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-const data = await response.json()
-document.body.style.backgroundImage = `url(${data.urls.full})`
-document.getElementById("author").textContent = `By: ${data.user.name}`
-
-  .catch(error => {
+try {
+    const res = await fetch("https:apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    const data = await response.json()
+    document.body.style.backgroundImage = `url(${data.urls.full})`
+    document.getElementById("author").textContent = `By: ${data.user.name}`  
+}catch(error) {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrbg&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
 
     document.getElementById("author").textContent = `By: Dodi Achmad`
-  })
+}
 
-const res = await fetch("https://api.coingecko.com/reference/coins/bitcoin")
+try{
+    const res = await fetch("https://api.coingecko.com/reference/coins/bitcoin")
 if (!response.ok) {
     throw Error("Something went wrong")
 }
@@ -22,18 +23,17 @@ document.getElementById("crypto").innerHTML = `
     <p>🎯: R${data.market_data.current_price.zar}</p>
     <p>👆: R${data.market_data.high_24h.zar}</p>
     <p>👇: R${data.market_data.low_24h.zar}</p> `
-
-    .catch(error => console.error(error))
+}catch(error) {console.error(error)}
 
 Function getCurrentTime() {
     const date = new Date()
     document.getElementrById("time").textContent = date.toLocaleTimeString("en-zar", {timeStyle: "short"})
 }
-
     setInterval(getCurrentTime, 1000)
 
 navigator.geolocation.getCurrentPosition(async position => {
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid`)
+    try{
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid`)
     if (!response.ok) {
         throw Error("Weather data not available")
     }
@@ -44,6 +44,6 @@ navigator.geolocation.getCurrentPosition(async position => {
         <p class="weather-temp">${Math.round(data.main.temp)}°C</p>
         <p class="weather-city">${data.name}</p>
     `
-        .catch(error => console.error(error)) 
+    }catch(error) {console.error(error)}
         })
     
