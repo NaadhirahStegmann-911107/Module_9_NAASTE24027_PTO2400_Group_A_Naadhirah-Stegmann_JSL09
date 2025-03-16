fc3c4a3609f33faf5919ad3184e711eb75aef684
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-03-16 21:25:14 +0200

    Crpypto Data
    Added a fail-safe in the event of receiving an error in retrieving information.
    
    Also, added the data to display the Bitcoin data and image as well as styling.

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ fetch("https:apis.scrimba.com/unsplash/photos/random?orientation=landscape&query
   })
   .catch(error => {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrbg&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
+
+    document.getElementById("author").textContent = `By: Dodi Achmad`
   })
+
+  fetch("https://api.coingecko.com/reference/coins/bitcoin")
+    .then(response => {
+        if (!response.ok) {
+            throw Error("Something went wrong")
+        }
+        return response.json()
+    })
+    .then(data => {
+        document.getElementById("cryto-top").innerHTML =`
+        <img src="${data.image.small}" />
+        <span>${data.name}</span>`
+    })
+        console.log(data)
+    .catch(error => console.error(error))
