f6136cad759af98e4715a85ea1db0268438e2eda
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-03-16 18:44:15 +0200

    Promise rejection
    Added a scenario that if the promise was rejected that the background image would have a default image in the event of.
    
    Added <p>-tags to divs and information to the relevant tags

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 fetch("https:apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
   .then(response => response.json())
   .then(data => {
+    console.log(data.urls.regular)
     document.body.style.backgroundImage = `url(${data.urls.full})`
     document.getElementById("author").textContent = `By: ${data.user.name}`
+  })
+  .catch(error => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrbg&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
   })
