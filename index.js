e2cad594e80719a9f91587b0164b7ba86414d601
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-03-17 07:19:30 +0200

    Debugging
    Attempted to edit based on Zip file from Scrimba and no changes on my end.

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
-try {
-    const res = await fetch("https:apis/unsplash/photos/random?orientation=landscape&query=nature")
-    const data = await response.json()
-    document.body.style.backgroundImage = `url(${data.urls.full})`
+fetch("https://apis/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+    document.body.style.backgroundImage = `url(${data.urls.regular})`
     document.getElementById("author").textContent = `By: ${data.user.name}`  
-}catch(error) {
-    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrbg&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`
+    })
+    .catch(err => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`
 
     document.getElementById("author").textContent = `By: Dodi Achmad`
-}
+})
 
-try{
-    const res = await fetch("https://api.coingecko.com/reference/coins/bitcoin")
-if (!response.ok) {
-    throw Error("Something went wrong")
-}
-const data = await response.json()
-document.getElementById("crypto-top").innerHTML =`
-    <img src="${data.image.small}" />
-    <span>${data.name}</span>`
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+    }
+    return res.json()
+    })
+    .then(data => {
+        document.getElementById("crypto-top").innerHTML =`
+            <img src="${data.image.small}" />
+            <span>${data.name}</span>`
 
-document.getElementById("crypto").innerHTML = `
-    <p>🎯: R${data.market_data.current_price.zar}</p>
-    <p>👆: R${data.market_data.high_24h.zar}</p>
-    <p>👇: R${data.market_data.low_24h.zar}</p> `
-}catch(error) {console.error(error)}
+        document.getElementById("crypto").innerHTML += `
+            <p>🎯: R${data.market_data.current_price.zar}</p>
+            <p>👆: R${data.market_data.high_24h.zar}</p>
+            <p>👇: R${data.market_data.low_24h.zar}</p> `
+})
+.catch(err => console.error(err))
 
 function getCurrentTime() {
     const date = new Date()
-    document.getElementById("time").textContent = date.toLocaleTimeString("en-zar", {timeStyle: "short"})
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-za", {timeStyle: "short"})
 }
     setInterval(getCurrentTime, 1000)
 
-navigator.geolocation.getCurrentPosition(async position => {
-    try{
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid`)
-    if (!response.ok) {
-        throw Error("Weather data not available")
-    }
-    const data = await response.json()
-    const iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+navigator.geolocation.getCurrentPosition(position => 
+    {
+        fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+            .then(res => {
+                if (!response.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+            })
+            .then(data => {
+                const iconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
     document.getElementById("weather").innerHTML =`
-        <img src=${iconURL} ?>
+        <img src=${iconURL} />
         <p class="weather-temp">${Math.round(data.main.temp)}°C</p>
         <p class="weather-city">${data.name}</p>
     `
-    }catch(error) {console.error(error)}
-        })
+    })
+    .catch(err => console.error(err))
+});
     
